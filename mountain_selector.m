cc551a0777ed7a85479d6d4541ca4413ac4cb75a
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2015-04-15 22:31:02 +0800

    find peaks

--- mountain_selector.m
+++ mountain_selector.m
@@ -10,7 +10,7 @@
 %      X                         % The data to count
 %      n                         % The number of bins in the histogram
 %
-function mountains= mountain_selector(NN, XX, extremes, number_of_bins) %   Area of a histogram
+function mountains= mountain_selector(NN, XX, number_of_bins) %   Area of a histogram
 %~~~~~~~~~~~~~~~~~~~~~~~~~
 % Compute total histogram size of c1n
 At = hist_size(NN);
@@ -23,49 +23,11 @@ good_mountains = [ ];
 leftValleyIdx = 1;
 rightValleyIdx = number_of_bins;
 
+[pks, idx] = findpeaks(NN);
+figure;
+plot(XX, NN, XX(idx), NN(idx), ".m");
+title("Mountains");
+
 % the minimas and maximas acquired from the histogram
-maximas = extremes(1:128);
-minimas = extremes(129:256);
 
-% find the first valley
-for i = 1:number_of_bins
-  % skip all non-valleys (mountains)
-  if (minimas(i) == 0)
-  	continue;
-  endif	
-
-  % found left valley
-  leftValleyIdx = i;
-
-  % beginning there, find the next valley
-  for j = (leftValleyIdx+1):number_of_bins
-    % skip all non-valleys (mountains)
-  	if(minimas(j) == 0)
-      continue;
-    endif
-  endfor
-
-  % found right valley
-  rightValleyIdx = j;
-
-
-  % Compute area of histogram between these valleys
-  Ap = hist_size(NN, leftValleyIdx, rightValleyIdx);
-
-  % Compute fwhm of this mountain
-  fwhmValue = fwhm(XX(leftValleyIdx:rightValleyIdx), NN(leftValleyIdx:rightValleyIdx));
-
-  % Avoid division by zero
-  if (fwhmValue > 0)
-    f = ( Ap / At ) * (100 / fwhmValue);
-
-    % insert into list
-    good_mountains = [good_mountains ; f leftValleyIdx rightValleyIdx];
-  endif
-
-  % continue to the next mountain beginning at the right valley as the left valley
-  i = j;
-endfor
-mountains=good_mountains;
-
-
+mountains = [pks, idx];
